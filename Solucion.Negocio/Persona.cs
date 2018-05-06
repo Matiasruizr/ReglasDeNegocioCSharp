27e6a7ded614d456d5e48e36d5fe2ccf7ee22169
matias <matiasruizr@hotmail.es>
2018-05-06 00:04:08 -0300

    Reglas de negocio para CSharp

---
+++ Solucion.Negocio/Persona.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucion.Negocio
+{
+    //Implementar reglas de negocio en las clases
+
+    public class Persona
+    {
+        private string _nombre;
+        private DateTime _fechaNacimiento;
+        private long _rut;
+        private string _digitoVerificador;
+
+     
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set {
+                if(value == String.Empty)
+                {
+                    //Arrojar nueva ExcepcionPersonalizada
+                    throw new ArgumentException("No se puede ingresar un nombre vacio");
+                }
+                else if(value.Trim().Length < 2)
+                {
+                    throw new ArgumentException("El nombre no puede tener menos de 2 caracteres");
+                }
+                else
+                {
+                    _nombre = value;
+                }
+            }
+        }
+
+
+        public DateTime FechaNacimiento
+        {
+            get { return _fechaNacimiento; }
+            set {
+                if(value > DateTime.Now)
+                {
+                    throw new ArgumentException("No puede ingresar una fecha mayor a hoy");
+                }
+                else
+                {
+                    _fechaNacimiento = value;
+                }
+            }
+        }
+
+
+        public long Rut
+        {
+            get { return _rut; }
+            set {
+                if (value <= 0 && value >= 100000000)
+                {
+                    throw new ArgumentException("El rut es incorrecto");
+                }
+                else
+                {
+                    _rut = value;
+                }
+            }
+        }
+
+        public string DigitoVerificador
+        {
+            get { return _digitoVerificador; }
+            set {
+                if (value.Trim() == string.Empty)
+                {
+                    throw new ArgumentException("Debe ingresar un digito verificador");
+                }
+                else
+                {
+                    _digitoVerificador = value;
+                }
+            }
+        }
+
+        public Persona(string nombre, long rut, string dv, DateTime fechaNacimiento)
+        {
+            Nombre = nombre;
+            Rut = rut;
+            DigitoVerificador = dv;
+            FechaNacimiento = fechaNacimiento;
+        }
+        public Persona()
+        {
+            _nombre = "Nombre";
+            _rut = 1;
+            _digitoVerificador = "j";
+            _fechaNacimiento = new DateTime(1990,12,10);
+        }
+    }
+}
---
+++ Solucion.Vista/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Solucion.Negocio;
+
+namespace Solucion.Vista
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValida_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string nombre = txtNombre.Text;
+                string dv = txtDigitoVerificador.Text;
+                DateTime fechaNacimiento = dtpFechaNacimiento.SelectedDate.Value;
+                long rut = long.Parse(txtRut.Text);
+
+                Persona p = new Persona(nombre,rut,dv,fechaNacimiento);
+
+                MessageBox.Show("Se ha instanciado la persona");
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+          
+        }
+    }
+}
